a47f2e11c6918a811138a43dcd6d33e01fcb77fa
Chris Johnson <cxjohnson@gmail.com>
2023-03-04 21:50:12 -0600

    Attempt to fix Facade.
    Also see how the Spatie PackageServiceProvider works.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Cxj\LookingGlass;
 
 use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Console\AboutCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Cxj\LookingGlass\Commands\LookingGlassCommand;
@@ -21,18 +22,40 @@ class ServiceProvider extends PackageServiceProvider
         $package
             // ->name('laravel-looking-glass-client')
             ->name('laravel-looking-glass')
-            ->hasConfigFile()
+            ->hasConfigFile('looking-glass.php')
             ->hasCommand(LookingGlassCommand::class);
     }
 
+    public function register()
+    {
+        $this->app->bind('LookingGlass', function($app) {
+            return new LookingGlass();
+        });
+    }
+
+
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__ . '/../config/looking-glass.php' => config_path(
-                'looking-glass.php'
-            ),
-        ]);
+        // Publish config file.
+//        $this->publishes([
+//            __DIR__ . '/../config/looking-glass.php' => config_path(
+//                'looking-glass.php'
+//            ),
+//        ]);
 
+        // Register commands.
+        AboutCommand::add(
+            'Looking Glass Client',
+            fn() => ['Version' => '0.0.1']
+        );
+
+//        if ($this->app->runningInConsole()) {
+//            $this->commands([
+//                LookingGlassCommand::class,
+//            ]);
+//        }
+
+        // Make dependecy package(s) available.
         $this->app->register(WebhookServerServiceProvider::class);
         $loader = AliasLoader::getInstance();
         $loader->alias('WebhookCall', WebhookCall::class);
