5314715ffa5088b86bfe6a390b2fe1fe23a36205
Chris Johnson <cxjohnson@gmail.com>
2023-03-06 16:43:56 -0600

    Revise for common case, remove unused params.

--- config/looking-glass.php
+++ config/looking-glass.php
@@ -2,8 +2,6 @@
 
 // config for Cxj/LookingGlass
 return [
-    'url'    => env('LOOKING_GLASS_URL', 'localhost:8000/webhook'),
-    'name'   => env('LOOKING_GLASS_NAME', 'Test App'),
-    'label'  => env('LOOKING_GLASS_LABEL', 'Test Label'),
+    'url'    => env('LOOKING_GLASS_URL', 'localhost:8000/api/webhook'),
     'secret' => env('LOOKING_GLASS_SECRET', 'shared-secret'),
 ];
